08cef84cb4265936aaab9bb8ab4cb29db15ab89c
fmway <fm18lv@gmail.com>
2026-09-15 22:11:42 +0700

    Lock import-tree

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
   outputs = inputs: import ./outputs.nix inputs;
 
-  inputs.import-tree.url = "github:denful/import-tree";
+  inputs.import-tree.url = "github:denful/import-tree/4ebb10ae17d5f1ad366e7aef5b92cb8eecf24f69";
 }
